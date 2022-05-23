324a18f77f4a073d550bc8ec443c7d38e343a6f9
Joseph Michael Becerra <becerra.joseph424@gmail.com>
2022-05-22 21:04:48 -0700

    stuck on my attempt to add a popup to the line feature 13.5.5

--- static/js/logic.js
+++ static/js/logic.js
@@ -30,9 +30,9 @@ console.log("working");
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
 //^basic google map view
 //dark view --> dark-v10
-//google maps --> streets-v11
+//google maps --> streets-v11 is the same as light-v10?
 //night preview navigation --> navigation-preview-night-v2
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -41,8 +41,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // streets.addTo(map);
 
 
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the night navigation view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v2/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -50,15 +50,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -69,15 +69,32 @@ L.control.layers(baseMaps).addTo(map);
 let airportData = "https://raw.githubusercontent.com/jojosaurusrex/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+    // pointToLayer: function(feature, latlng) {
+    //     console.log(feature);
+    //     return L.marker(latlng)
+    //     .bindPopup("<h2> Airport Code: " + feature.properties.faa + 
+    //     "</h2> <hr> <h4> Airport name: " + feature.properties.name + "</h4>");
+    //   }
+//   }).addTo(map);
+// });
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/jojosaurusrex/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-    pointToLayer: function(feature, latlng) {
+      onEachFeature: function(feature, latlng) {
         console.log(feature);
         return L.marker(latlng)
-        .bindPopup("<h2> Airport Code: " + feature.properties.faa + 
-        "</h2> <hr> <h4> Airport name: " + feature.properties.name + "</h4>");
+        .bindPopup("<h2> Airline: " + feature.properties.airline + 
+        "</h2> <hr> <h4> Destination: " + feature.properties.dst + "</h4>");
       }
   }).addTo(map);
 });
